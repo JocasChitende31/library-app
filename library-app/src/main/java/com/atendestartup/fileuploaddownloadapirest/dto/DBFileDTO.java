d661ff215985ec55f1cea3268ea16620dc71c07e
JeoChtende31 <84420296+JocasChitende31@users.noreply.github.com>
2024-02-27 13:10:34 +0100

    Delete library-app directory

--- library-app/src/main/java/com/atendestartup/fileuploaddownloadapirest/dto/DBFileDTO.java
+++
@@ -1,4 +0,0 @@
-package com.atendestartup.fileuploaddownloadapirest.dto;
-
-public record DBFileDTO(String fileName, String fileType, byte[] data) {
-}
--- library-app/src/main/java/com/atendestartup/fileuploaddownloadapirest/dto/UploadFileResponse.java
+++
@@ -1,4 +0,0 @@
-package com.atendestartup.fileuploaddownloadapirest.dto;
-
-public record UploadFileResponse(String fileName, String downloadUri, String contentType, long size) {
-}
